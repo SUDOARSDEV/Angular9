8aef6394b2fe331207ef66423624b9cfe8d1bd3f
SUDOARSDEV <sheikharsalanmajeed@gmail.com>
2021-07-06 12:25:30 +0500

    Angular Updatation 12

--- src/app/components/Security/verification/verification.component.spec.ts
+++ src/app/components/Security/verification/verification.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VerificationComponent } from './verification.component';
 
@@ -6,7 +6,7 @@ describe('VerificationComponent', () => {
   let component: VerificationComponent;
   let fixture: ComponentFixture<VerificationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VerificationComponent ]
     })
